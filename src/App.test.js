762117a8de26a72cf18937178595456b64874e5a
Selvaganapathi Kannan <selvaganapathi@mallow-tech.com>
2020-12-25 00:06:04 +0530

    updated testcases

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,29 @@
 import { tuvaEditor, isbold } from './utility'
 
-test('it should return paragraph tag', () => {
+test('it should return paragraph tag : 1', () => {
   expect(tuvaEditor('selva')).toBe('<p>selva</p>')
 });
 
-test('it should return strong tag', () => {
+test('it should return paragraph tag : 2', () => {
+  expect(tuvaEditor('<p>selva</p>')).toBe('<p><p>selva</p></p>')
+});
+
+test('it should return strong tag : 1', () => {
   expect(isbold('**selva**')).toBe('<strong>selva</strong>')
 });
 
-test('it should return paragraph with strong tag', () => {
+test('it should return strong tag : 2', () => {
+  expect(isbold('***selva**')).toBe('<strong>*selva</strong>')
+});
+
+test('it should return paragraph with strong tag : 3', () => {
   expect(tuvaEditor('hi selva **Good morning**')).toBe('<p>hi selva <strong>Good morning</strong></p>')
+});
+
+test('it should return paragraph with strong tag astrisk too : 4', () => {
+  expect(tuvaEditor('hi selva **Good * * morning**')).toBe('<p>hi selva <strong>Good * * morning</strong></p>')
+});
+
+test('it should return paragraph without strong tag : 5', () => {
+  expect(tuvaEditor('hi selva **Good * * morning')).toBe('<p>hi selva **Good * * morning</p>')
 });
