ef9e5d7c9feaf56c432c231bbd4c2a836acde2dd
Selvaganapathi Kannan <selvaganapathi@mallow-tech.com>
2020-12-24 23:48:30 +0530

    allow to add a extra button through props

--- src/utility.js
+++ src/utility.js
@@ -1,3 +1,5 @@
+import { insertBold, insertItalic, insertUnorderedList, insertStrikeThrough } from '../src/core/browserAPI'
+
 export function tuvaEditor(text) {
   let paragraph_rejax = /(?=^|>|\n)\s*\n+([^<]+?)\n+\s*(?=\n|<|$)/g;
   text = '\n' + text + '\n'; // to check end of line
@@ -12,12 +14,6 @@ export function isbold(text) {
   return html;
 }
 
-// doc API
-
-let insertBold = () => document.execCommand('bold', false, null)
-let insertItalic = () => document.execCommand('italic', false, null)
-let insertUnorderedList = () => document.execCommand('insertUnorderedList', false, null)
-
 
 export const buttons = [{
   type: 'bold',
@@ -26,14 +22,22 @@ export const buttons = [{
     insertBold()
   }
 }, {
+  type: 'italic',
+  svgIcon: <svg data-name="italic" width="12" height="12" role="img" viewBox="0 0 320 512"><path fill="currentColor" d="M204.758 416h-33.849l62.092-320h40.725a16 16 0 0 0 15.704-12.937l6.242-32C297.599 41.184 290.034 32 279.968 32H120.235a16 16 0 0 0-15.704 12.937l-6.242 32C96.362 86.816 103.927 96 113.993 96h33.846l-62.09 320H46.278a16 16 0 0 0-15.704 12.935l-6.245 32C22.402 470.815 29.967 480 40.034 480h158.479a16 16 0 0 0 15.704-12.935l6.245-32c1.927-9.88-5.638-19.065-15.704-19.065z"></path></svg>,
+  execCommand: function excuteCommand() {
+    insertItalic()
+  },
+}, {
+  type: 'strikeThrough',
+  svgIcon: <svg data-name="strikethrough" width="12" height="12" role="img" viewBox="0 0 512 512"><path fill="currentColor" d="M496 288H16c-8.837 0-16-7.163-16-16v-32c0-8.837 7.163-16 16-16h480c8.837 0 16 7.163 16 16v32c0 8.837-7.163 16-16 16zm-214.666 16c27.258 12.937 46.524 28.683 46.524 56.243 0 33.108-28.977 53.676-75.621 53.676-32.325 0-76.874-12.08-76.874-44.271V368c0-8.837-7.164-16-16-16H113.75c-8.836 0-16 7.163-16 16v19.204c0 66.845 77.717 101.82 154.487 101.82 88.578 0 162.013-45.438 162.013-134.424 0-19.815-3.618-36.417-10.143-50.6H281.334zm-30.952-96c-32.422-13.505-56.836-28.946-56.836-59.683 0-33.92 30.901-47.406 64.962-47.406 42.647 0 64.962 16.593 64.962 32.985V136c0 8.837 7.164 16 16 16h45.613c8.836 0 16-7.163 16-16v-30.318c0-52.438-71.725-79.875-142.575-79.875-85.203 0-150.726 40.972-150.726 125.646 0 22.71 4.665 41.176 12.777 56.547h129.823z"></path></svg>,
+  execCommand: function excuteCommand() {
+    insertStrikeThrough()
+  },
+},
+{
   type: 'unorderlist',
   svgIcon: <svg data-name="unordered-list" width="12" height="12" viewBox="0 0 512 512"><path fill="currentColor" d="M96 96c0 26.51-21.49 48-48 48S0 122.51 0 96s21.49-48 48-48 48 21.49 48 48zM48 208c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48-21.49-48-48-48zm0 160c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48-21.49-48-48-48zm96-236h352c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H144c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h352c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H144c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h352c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H144c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"></path></svg>,
   execCommand: function excuteCommand() {
     insertUnorderedList()
   },
-},
-{
-  type: 'italic',
-  svgIcon: <svg data-name="italic" width="12" height="12" role="img" viewBox="0 0 320 512"><path fill="currentColor" d="M204.758 416h-33.849l62.092-320h40.725a16 16 0 0 0 15.704-12.937l6.242-32C297.599 41.184 290.034 32 279.968 32H120.235a16 16 0 0 0-15.704 12.937l-6.242 32C96.362 86.816 103.927 96 113.993 96h33.846l-62.09 320H46.278a16 16 0 0 0-15.704 12.935l-6.245 32C22.402 470.815 29.967 480 40.034 480h158.479a16 16 0 0 0 15.704-12.935l6.245-32c1.927-9.88-5.638-19.065-15.704-19.065z"></path></svg>,
-  execCommand: function excuteCommand() { insertItalic() }
 }]
