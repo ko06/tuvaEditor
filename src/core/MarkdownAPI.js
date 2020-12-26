13f736846ebf26d5ff3b00d75807ae085a5c6fb7
Selvaganapathi Kannan <selvaganapathi@mallow-tech.com>
2020-12-26 12:39:15 +0530

    string issue fixed and additional h tags added

--- src/core/MarkdownAPI.js
+++ src/core/MarkdownAPI.js
@@ -1,18 +1,18 @@
 export function tuvaMarkdown(src) {
+  src = '\n' + src + '\n'
   src = checkBold(src)
   src = checkItalic(src)
   src = checkHeader(src)
   src = checkAtag(src)
-  src = checkBlockquote(src)
+  // src = checkBlockquote(src)
   src = checkAtag(src)
   src = checkHRrule(src)
-
+  src = checkCodeBlock(src)
   return src;
 }
 
 export function checkHRrule(src) {
-  return src
-  //.replace(/^([*\-=_] *){3,}$/gm, '<hr/>')
+  return src.replace(/\n((\-{3,})|(={3,}))/g, "\n<hr/>")
 }
 
 export function checkImage(src) {
@@ -32,11 +32,18 @@ export function checkAtag(src) {
 }
 
 export function checkHeader(src) {
-  return src.replace(/^### (.*$)/gim, '<h3>$1</h3>')
-    .replace(/^## (.*$)/gim, '<h2>$1</h2>')
-    .replace(/^# (.*$)/gim, '<h1>$1</h1>')
+  return src.replace(/^###### (.*$)/gim, '\n<h6>$1</h6>')
+    .replace(/^##### (.*$)/gim, '\n<h5>$1</h5>')
+    .replace(/^#### (.*$)/gim, '\n<h4>$1</h4>')
+    .replace(/^### (.*$)/gim, '\n<h3>$1</h3>')
+    .replace(/^## (.*$)/gim, '\n<h2>$1</h2>')
+    .replace(/^# (.*$)/gim, '\n<h1>$1</h1>')
 }
 
 export function checkBlockquote(src) {
-  return src.replace(/^\> (.*$)/gim, '<blockquote>$1</blockquote>')
+  return src.replace( /\n(&gt;|\>)(.*)/g , '\n<blockquote>$1</blockquote>')
+}
+
+export function checkCodeBlock(src){
+  return src.replace(/`([^`]+)`/g, '\n<code>$1</code>');
 }
